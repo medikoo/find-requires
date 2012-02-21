3c8378cc832ced3c8a5467cd25b8e667c84869a4
Mariusz Nowak <mariusz@medikoo.com>
2012-02-21 15:34:43 +0100

    -- whitespace

--- test/__playground/edge.js
+++ test/__playground/edge.js
@@ -2,17 +2,17 @@ require('on\u0065')
 require(12)
 var foo = require('thr/ee').b.c.d().e.f().g
 if (foobar) {
-  var bla = function() {
-    switch (baz) {
-      default:
-        require("fo\\ur")()()()()
-    }
-  }
-  qux(1, 3, quux(require('five').bar.foo, 4), 2)
+	var bla = function() {
+		switch (baz) {
+		default:
+			require("fo\\ur")()()()()
+		}
+	}
+	qux(1, 3, quux(require('five').bar.foo, 4), 2)
 }
 
 function func() {
-  return
+	return
 }
 
 require(baz);
@@ -21,28 +21,28 @@ var str = "raz; require('string'); ";
 
 var str2 = 'raz; require("string2");';
 var a = b
-/require("six")/g;
+	/require("six")/g;
 
 a['raz']
-/require("seven")/g
+	/require("seven")/g
 
 var c = d;
-/require('regexp');/g
+	/require('regexp');/g
 
 require()
 
 if (a) {
 }
-/require("reqexp2")/
+	/require("reqexp2")/
 
 var x = {
-  raz: 'dwa'
+	raz: 'dwa'
 } /require("eight");
 
 obj.require('object');
 
 obj.
-require("object2");
+	require("object2");
 
 // require('comment');
 var raz = 4;  // require('comment2')
@@ -50,20 +50,20 @@ var raz = 4;  // require('comment2')
 /* require('mlcomment'); */
 
 function test() {
-  return require('nine');
+	return require('nine');
 }
 
 /* comment
 
-require('mlcomment2');
+ require('mlcomment2');
 
-*/
+ */
 
 if (foo) { } require('ten');
 
 
 require('eleven' + 'split' +
-  'path');
+	'path');
 
 require(true ? "twelve" : "bar");
 
@@ -73,49 +73,49 @@ require.foo;
 
 
 function test() {
-  a();return    require(('four') + 'teen');
+	a();return    require(('four') + 'teen');
 }
 
 
 function test() {
-return require('fifteen');
+	return require('fifteen');
 }
 
 function test() {
- a(); return require('sixteen');
+	a(); return require('sixteen');
 }
 
 switch (require('seventeen')) {
-  case 1:
-  require("'eighteen'");
-  break;
-  case 2:
-  require('nineteen');
-  break;
-  default:
-  require('twenty');
+case 1:
+	require("'eighteen'");
+	break;
+case 2:
+	require('nineteen');
+	break;
+default:
+	require('twenty');
 }
 
 if (require('twenty/one')) {
 } else if (require('twenty/two')) {
-  require('twenty/three');
+	require('twenty/three');
 }
 
 for (var i, j = require('/twenty/two/2/'); require('twenty/three/2/');
-    require('twenty/four/2/\'')) {
-  require("twenty/five/2/\"");
+	require('twenty/four/2/\'')) {
+	require("twenty/five/2/\"");
 }
 
 for (require('\'twenty/six') in require('\'twenty/seven\'')) {
-  require("\"twenty/eight");
+	require("\"twenty/eight");
 }
 
 with (a, require("\"twenty/nine\"")) {
-  require('"thirty"');
+	require('"thirty"');
 }
 
 require('thirty\
- break-line \
-one');
+	break-line \
+	one');
 
 module.exports = require('thirty\two')
