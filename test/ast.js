041d1c1f9d3c2352e3d9e06c8127af10e32ed6f8
Mariusz Nowak <mariusz@medikoo.com>
2012-02-21 15:35:06 +0100

    Update tests

--- test/ast.js
+++ test/ast.js
@@ -10,7 +10,7 @@ module.exports = function (t, a, d) {
 		'twenty', 'twenty/one', 'twenty/two', 'twenty/three', '/twenty/two/2/',
 		'twenty/three/2/', 'twenty/four/2/\'', 'twenty/five/2/"', '\'twenty/six',
 		'\'twenty/seven\'', '"twenty/eight', '"twenty/nine"', '"thirty"',
-		'thirty break-line one', 'thirty\two'];
+		'thirty\tbreak-line \tone', 'thirty\two'];
 
 	readFile(pg + '/edge.js', 'utf-8', function (err, str) {
 		var astR;
