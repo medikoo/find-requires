8300b9ae3eab7e0e1752fd27dc87a10721e2a94e
Mariusz Nowak <medyk@medikoo.com>
2016-11-24 18:16:39 -0600

    Add breaking test

--- test/index.js
+++ test/index.js
@@ -8,7 +8,7 @@ module.exports = function (t, a, d) {
 	var result = ['one', '12', 'thr/ee', 'fo\\ur', 'five', 'six', 'seven',
 		'undefined', 'nine', 'ten', 'elevensplitpath', 'twelve',
 		'fourteen', 'fifteen', 'sixteen', 'seventeen', '\'eighteen\'', 'nineteen',
-		'twenty', 'twenty/one', 'twenty/two', 'twenty/three', '/twenty/two/2/',
+		'twenty', 'elo', 'twenty/one', 'twenty/two', 'twenty/three', '/twenty/two/2/',
 		'twenty/three/2/', 'twenty/four/2/\'', 'twenty/five/2/"',
 		'\'twenty/seven\'', '"twenty/eight', '"twenty/nine"', '"thirty"',
 		'mid-thirty', 'marko', 'thirty\tbreak-line \tone', 'thirty\two'];
