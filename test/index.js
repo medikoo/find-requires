a2e10bdcdb4e77b5f537bbe8d2a7e0ccf55841ca
Mariusz Nowak <medyk@medikoo.com>
2018-12-28 14:11:56 +0100

    style: prettier

--- test/index.js
+++ test/index.js
@@ -1,17 +1,17 @@
 "use strict";
 
 var readFile = require("fs").readFile
-
-  , pg = __dirname + "/__playground";
+  , pg       = __dirname + "/__playground";
 
 module.exports = function (t, a, d) {
-	var result = ["one", "12", "thr/ee", "fo\\ur", "five", "six", "seven",
-		"undefined", "nine", "ten", "elevensplitpath", "twelve",
-		"fourteen", "fifteen", "sixteen", "seventeen", "'eighteen'", "nineteen",
-		"twenty", "elo", "twenty/one", "twenty/two", "twenty/three", "/twenty/two/2/",
-		"twenty/three/2/", "twenty/four/2/'", "twenty/five/2/\"",
-		"'twenty/seven'", "\"twenty/eight", "\"twenty/nine\"", "\"thirty\"",
-		"mid-thirty", "marko", "thirty\tbreak-line \tone", "thirty\two"];
+	var result = [
+		"one", "12", "thr/ee", "fo\\ur", "five", "six", "seven", "undefined", "nine", "ten",
+		"elevensplitpath", "twelve", "fourteen", "fifteen", "sixteen", "seventeen", "'eighteen'",
+		"nineteen", "twenty", "elo", "twenty/one", "twenty/two", "twenty/three", "/twenty/two/2/",
+		"twenty/three/2/", "twenty/four/2/'", "twenty/five/2/\"", "'twenty/seven'",
+		"\"twenty/eight", "\"twenty/nine\"", "\"thirty\"", "mid-thirty", "marko",
+		"thirty\tbreak-line \tone", "thirty\two"
+	];
 
 	readFile(pg + "/edge.js", "utf-8", function (err, str) {
 		var astR;
