77a9cc37ca90a88fd8060eac16cbc83f0ae6bd5d
Mariusz Nowak <medyk@medikoo.com>
2018-12-28 14:10:40 +0100

    style: prettier

--- index.js
+++ index.js
@@ -4,14 +4,9 @@ var value  = require("es5-ext/object/valid-value")
   , esniff = require("esniff/function")("require");
 
 module.exports = function (code/*, options*/) {
-	var options = Object(arguments[1])
-	  , deps = esniff(String(value(code)));
+	var options = Object(arguments[1]), deps = esniff(String(value(code)));
 	deps.forEach(function (data) {
-		try {
-			data.value = String(eval(data.raw));
-		} catch (ignore) {}
+		try { data.value = String(eval(data.raw)); } catch (ignore) {}
 	});
-	return options.raw ? deps : deps.map(function (dep) {
-		return dep.value;
-	}).filter(Boolean);
+	return options.raw ? deps : deps.map(function (dep) { return dep.value; }).filter(Boolean);
 };
