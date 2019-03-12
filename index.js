de463c84b52d42fef1e3d481d62156a32c755deb
Mariusz Nowak <medyk@medikoo.com>
2019-03-12 13:23:10 +0100

    feat: support setupCode option

--- index.js
+++ index.js
@@ -2,14 +2,17 @@
 
 const ensureString = require("es5-ext/object/validate-stringifiable-value")
     , isObject     = require("es5-ext/object/is-object")
+    , isValue      = require("es5-ext/object/is-value")
     , esniff       = require("esniff/function")("require");
 
 module.exports = function (code, options = {}) {
+	code = ensureString(code);
 	if (!isObject(options)) options = {};
-	const deps = esniff(ensureString(code));
+	const setupCode = isValue(options.setupCode) ? ensureString(options.setupCode) : ""
+	    , deps = esniff(code);
 	deps.forEach(data => {
 		let requirePath;
-		try { requirePath = eval(data.raw); }
+		try { requirePath = new Function(`${ setupCode }; return (${ data.raw });`)(); }
 		catch (ignore) {}
 		if (typeof requirePath === "number") requirePath = String(requirePath);
 		if (typeof requirePath === "string") data.value = requirePath;
