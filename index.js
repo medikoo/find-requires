c4822ef96a434eb246be58b248b5d6b94c9faf73
Mariusz Nowak <medyk@medikoo.com>
2019-03-12 13:00:19 +0100

    feat: improve options validation

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 "use strict";
 
 const ensureString = require("es5-ext/object/validate-stringifiable-value")
+    , isObject     = require("es5-ext/object/is-object")
     , esniff       = require("esniff/function")("require");
 
 module.exports = function (code, options = {}) {
-	options = Object(options);
+	if (!isObject(options)) options = {};
 	const deps = esniff(ensureString(code));
 	deps.forEach(data => {
 		let requirePath;
