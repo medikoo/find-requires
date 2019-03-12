83610c8971cf7bef4d85a0f47c789ec34ff2bd80
Mariusz Nowak <medyk@medikoo.com>
2019-03-12 12:53:47 +0100

    chore: upgrade to ES2015
    BREAKING CHANGE:
    Drop support to Node.js v4

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 "use strict";
 
-var ensureString = require("es5-ext/object/validate-stringifiable-value")
-  , esniff       = require("esniff/function")("require");
+const ensureString = require("es5-ext/object/validate-stringifiable-value")
+    , esniff       = require("esniff/function")("require");
 
-module.exports = function (code/*, options*/) {
-	var options = Object(arguments[1]), deps = esniff(ensureString(code));
-	deps.forEach(function (data) {
-		var requirePath;
-		try { requirePath = eval(data.raw); } catch (ignore) {}
+module.exports = function (code, options = {}) {
+	options = Object(options);
+	const deps = esniff(ensureString(code));
+	deps.forEach(data => {
+		let requirePath;
+		try { requirePath = eval(data.raw); }
+		catch (ignore) {}
 		if (typeof requirePath === "number") requirePath = String(requirePath);
 		if (typeof requirePath === "string") data.value = requirePath;
 	});
-	return options.raw ? deps : deps.map(function (dep) { return dep.value; }).filter(Boolean);
+	return options.raw ? deps : deps.map(dep => dep.value).filter(Boolean);
 };
--- test/index.js
+++ test/index.js
@@ -1,22 +1,22 @@
 "use strict";
 
-var resolve  = require("path").resolve
-  , readFile = require("fs").readFile;
+const { resolve }  = require("path")
+    , { readFile } = require("fs");
 
-var pg = resolve(__dirname, "__playground");
+const pg = resolve(__dirname, "__playground");
 
 module.exports = function (t, a, d) {
-	var result = [
-		"one", "12", "thr/ee", "fo\\ur", "five", "six", "seven", "nine", "ten",
-		"elevensplitpath", "twelve", "fourteen", "fifteen", "sixteen", "seventeen", "'eighteen'",
-		"nineteen", "twenty", "elo", "twenty/one", "twenty/two", "twenty/three", "/twenty/two/2/",
-		"twenty/three/2/", "twenty/four/2/'", "twenty/five/2/\"", "'twenty/seven'",
-		"\"twenty/eight", "\"twenty/nine\"", "\"thirty\"", "mid-thirty", "marko",
-		"thirty\tbreak-line \tone", "thirty\two"
+	const result = [
+		"one", "12", "thr/ee", "fo\\ur", "five", "six", "seven", "nine", "ten", "elevensplitpath",
+		"twelve", "fourteen", "fifteen", "sixteen", "seventeen", "'eighteen'", "nineteen", "twenty",
+		"elo", "twenty/one", "twenty/two", "twenty/three", "/twenty/two/2/", "twenty/three/2/",
+		"twenty/four/2/'", "twenty/five/2/\"", "'twenty/seven'", "\"twenty/eight",
+		"\"twenty/nine\"", "\"thirty\"", "mid-thirty", "marko", "thirty\tbreak-line \tone",
+		"thirty\two"
 	];
 
-	readFile(pg + "/edge.js", "utf-8", function (err, str) {
-		var astR;
+	readFile(`${ pg }/edge.js`, "utf-8", (err, str) => {
+		let astR;
 		if (err) {
 			d(err);
 			return;
@@ -24,7 +24,7 @@ module.exports = function (t, a, d) {
 		a.deep(t(str), result, "Plain result");
 
 		d({
-			"Raw option": function (a) {
+			"Raw option": a => {
 				astR = t(str, { raw: true });
 				a(astR[0].value, "one", "Value");
 				a(astR[0].point, 9, "Point");
